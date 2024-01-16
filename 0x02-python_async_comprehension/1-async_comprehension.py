e56b602add0f328f3ef6e5edad68fdc69ba938e0
Fistum1 <fistumitiku@gmail.com>
2024-01-16 14:31:49 +0000

    py-async-comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Async comprenhesion gen
+"""
+import asyncio
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+        It generates numbers with async comprenhension
+
+        Args:
+            void
+
+        Return:
+            float random numbers
+    """
+    return ([i async for i in async_generator()])
